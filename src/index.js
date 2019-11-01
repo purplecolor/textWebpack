c803da954ee545be74b7a9d78f74c1e76a48919b
yuang <13526739259@163.com>
2019-11-01 11:48:47 +0800

    '管理输出'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './img.jpg'
-import Data from "./data.xml"
+import printMe from './print.js';
 function component(){
     var element = document.createElement('div');
     element.innerHTML=_.join(['hello','webpack'])
-    element.classList.add('hello')
-    var myIcon =new Image();
-    myIcon.src=Icon
-    element.appendChild(myIcon)
-    console.log(Data)
+    var btn = document.createElement('button');
+    btn.innerHTML="点击我展示console!"
+    btn.onclick=printMe
+    element.appendChild(btn)
     return element;
 }
 document.body.appendChild(component())
