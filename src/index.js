8bb3b4640bdbf9f4acba79669341c18d6d41456d
yuang <13526739259@163.com>
2019-11-05 17:22:00 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import _ from 'lodash';
-function getComponent(){
-    return import(/* webpackChunkName:"lodash" */ 'lodash').then(_ =>{
-        var element=document.createElement('div');
-        element.innerHTML=_.join(['hello','webpack'],' ');
-        return element;
-    }).catch(error=>'An error occurred')
+function component(){
+    var element =document.createElement('div');
+    var button = document.createElement("button");
+    var br = document.createElement('br');
+    button.innerHTML='Click me and look at the console'
+    element.innerHTML=_.join(['Hello','webpack'],'');
+    element.appendChild(br)
+    element.appendChild(button);
+    button.onclick=e=>import('./print').then(module=>{
+        var print = module.aaa;
+        print()
+    })
+    return element;
 }
 document.body.appendChild(component())
-getComponent().then(component=>{
-    document.body.appendChild(component)
-})
