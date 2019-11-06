503a4aaaba7dc2931c1924425d0a563a75457dfc
yuang <13526739259@163.com>
2019-11-06 19:34:48 +0800

    ts指南

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import _ from 'lodash';
 import printMe from './print.js';
+if("serviceWorker" in navigator){
+    window.addEventListener('load',()=>{
+        navigator.serviceWorker.register('/service-worker.js').then(registration=>{
+            console.log('SW registered:',registration);
+        }).catch(registrationError=>{
+            console.log("SW registration failed:",registrationError)
+        })
+    })
+}
 function component(){
     var element = document.createElement('div');
     element.innerHTML=_.join(['hello','webpack'])
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
 const path=require('path');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 const {CleanWebpackPlugin}=require('clean-webpack-plugin');
+const WorkboxPlugin=require('workbox-webpack-plugin')
 module.exports={
-		entry:{
-			app:'./src/index.js',
-			print:'./src/print.js'
-		},
+		entry:'./src/index.js',
+		devtool:'inline-source-map',
 		output:{
 			filename:'[name].bundle.js',
 			path:path.resolve(__dirname,'dist')
 		},
 		plugins:[
 			new CleanWebpackPlugin(),
-			new HtmlWebpackPlugin({title:"Output Management"})
-		]
+			new HtmlWebpackPlugin({title:"Output Management"}),
+			new WorkboxPlugin.GenerateSW({
+				clientsClaim:true,
+				skipWaiting:true
+			})
+		],
+		module:{
+			rules:[
+				{
+					test:/\.tsx?$/,
+					use:'ts-loader',
+					exclude:/node_modules/
+				}
+			]
+		},
+		resolve:{
+			extensions:['.tsx','ts','.js']
+		}
 }
