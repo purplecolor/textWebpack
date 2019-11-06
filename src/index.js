e17dd381abc10e546ee537f7e997edd2ec14b76a
yuang <13526739259@163.com>
2019-11-06 13:28:22 +0800

    缓存

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import print from './print.js';
 function component(){
     var element = document.createElement('div');
-    element.innerHTML=_.join(['hello','webpack'])
-    var btn = document.createElement('button');
-    btn.innerHTML="点击我展示console!"
-    btn.onclick=printMe
-    element.appendChild(btn)
+    element.innerHTML=_.join(['hello','webpack'],'')
+    element.onclick=print.bind(null,'hello webpack!')
     return element;
 }
 document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,30 @@
 const path=require('path');
+const webpack = require('webpack')
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 const {CleanWebpackPlugin}=require('clean-webpack-plugin');
 module.exports={
 		entry:{
-			app:'./src/index.js',
-			print:'./src/print.js'
+			main:'./src/index.js',
+			vendor:['lodash']
 		},
 		output:{
-			filename:'[name].bundle.js',
+			filename:'[name].[chunkhash].js',
 			path:path.resolve(__dirname,'dist')
 		},
 		plugins:[
 			new CleanWebpackPlugin(),
-			new HtmlWebpackPlugin({title:"Output Management"})
-		]
+			new HtmlWebpackPlugin({title:"Caching"}),
+			new webpack.HashedModuleIdsPlugin()
+		],
+		optimization:{
+			splitChunks:{
+				cacheGroups:{
+					commons:{
+						name:"lodash",
+						chunks:"initial",
+						minChunks:1
+					}
+				}
+			}
+		}
 }
